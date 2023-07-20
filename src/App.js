3d3d9c7df6a0895a17565c41fd4e2393f58bfc53
aim <ahmadhakim404@gmail.com>
2023-07-20 21:47:26 +0700

    sembunyikan dan tampilkan form tambah teman

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -19,13 +21,25 @@ const initialFriends = [
   },
 ];
 
+
+
+function Button({ children, onClick }) {
+  return <button className="button" onClick={onClick}>{children}</button>
+}
+
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((showAddFriend) => !showAddFriend);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendList />
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        {showAddFriend && (<FormAddFriend />)}
+        <Button onClick={handleShowAddFriend}>{showAddFriend ? 'Close' : 'Add Friend'}</Button>
       </div>
       <FormSplitBill />
     </div>
@@ -102,9 +116,5 @@ function FormSplitBill() {
       <Button>Add</Button>
     </form>
   )
-}
-
-function Button({ children }) {
-  return <button className="button">{children}</button>
 }
 
